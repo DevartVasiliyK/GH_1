fb4b506c7b65c4c0684c06a56559e7f7406c8d78
datasoft11111 <datasoft11111>
2019-01-28 08:05:13 +0200

    f

---
+++ Tables/dbo.AccountProfiles.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[AccountProfiles] (
+  [AccountId] [int] NOT NULL,
+  [ExternalCustomerId] [uniqueidentifier] NOT NULL,
+  [AccountName] [nvarchar](128) NULL,
+  [CreatedAt] [date] NULL,
+  [OwnerName] [nvarchar](128) NULL,
+  [OwnerEmail] [nvarchar](128) NULL,
+  [SubscriptionPlan] [nvarchar](128) NULL,
+  [SubscriptionReference] [nvarchar](50) NULL,
+  [IsTrialUsed] [bit] NOT NULL DEFAULT (0),
+  [HasPurchases] [bit] NULL DEFAULT (0),
+  [ActiveMembers] [int] NULL,
+  [LockedMembers] [int] NULL,
+  [LockedBySubscriptionMembers] [int] NULL,
+  [LastTimeEntryDate] [date] NULL
+)
+GO
